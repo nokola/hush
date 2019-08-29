d59b5d4785b7a3f42159d49282adcdb15ed06402
Nokola <nikolami@outlook.com>
2019-08-29 01:31:56 -0700

    Basic autoreload server

---
+++ readme.md
@@ -0,0 +1,8 @@
+## Running the Server
+From `https://actix.rs/docs/autoreload/`
+
+To now run the development server invoke this command:
+
+```
+systemfd --no-pid -s http::3000 -- cargo watch -x run
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use listenfd::ListenFd;
+
+#[get("/")]
+fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[get("/test")]
+fn index2() -> impl Responder {
+    HttpResponse::Ok().body("Hello world again!")
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Listening on http://localhost:3000");
+    let mut listenfd = ListenFd::from_env();
+    let mut server = HttpServer::new(|| App::new()
+        .service(index)
+        .service(index2));
+        // .route("/", web::get().to(index)));
+
+    server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
+        server.listen(l).unwrap()
+    } else {
+        server.bind("127.0.0.1:3000").unwrap()
+    };
+
+    server.run().unwrap();
 }
